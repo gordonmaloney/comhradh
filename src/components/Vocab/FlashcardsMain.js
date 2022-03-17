1b43d5687988d6f476b7aebdbb7e4d12ba2ea5b3
Gordon Maloney <gordonmaloney@gmail.com>
2022-03-17 15:48:20 +0000

    flashcard deck for each level

--- src/components/Vocab/FlashcardsMain.js
+++ src/components/Vocab/FlashcardsMain.js
@@ -6,19 +6,26 @@ import { Studier } from './Studier'
 export const FlashcardsMain = () => {
 
     const user = "testUser"
-    const userLevel = 2
+    const userLevel = 1
 
     const words = []
     WORDS.map(lesson => {lesson.lesson <= userLevel && lesson.words.map(word => words.push(word))})
     console.log(words)
 
+    //level 1 vocab
+    const level1vocab = WORDS.filter(word => word.lesson == 1)[0].words
+    console.log(level1vocab)
+    //level2 vocab
+    const level2vocab = WORDS.filter(word => word.lesson == 2)[0].words
+    console.log(level2vocab)
+
   return (
     <div className="innerContainer">
         <h1>Flashcards</h1>
-        <span>{user} - level {userLevel}</span>
-
-        <Studier words={words}/>
-
+        <span>Level 1:</span>
+        <Studier words={level1vocab}/>
+        <span>Level 2:</span>
+        <Studier words={level2vocab}/>
     </div>
   )
 }
--- src/components/Vocab/Studier.js
+++ src/components/Vocab/Studier.js
@@ -1,10 +1,76 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export const Studier = ({words}) => {
+//take words from prop
+//shuffle them
+//push duplicate to end of deck if wrong
+//activeStep progresses by one w/ each guess
+
+export const Studier = ({ words }) => {
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  const [studyList, setStudyList] = useState(shuffle(words));
+
+  const [activeCard, setActiveCard] = useState(0);
+
+  const [reveal, setReveal] = useState(false);
+
+  const [revise, setRevise] = useState([""]);
+
+  const handleRight = () => {
+    setReveal(false);
+    setActiveCard(activeCard + 1);
+  };
+
+  const handleWrong = () => {
+    setStudyList([...studyList, studyList[activeCard]]);
+    setReveal(false);
+    setActiveCard(activeCard + 1);
+  };
+
+  console.log();
 
   return (
     <>
-    {words.map(word => <>{word.en} - {word.gd}<br /></>)}
+      {studyList.length != activeCard ? (
+        <div style={{ border: "1px solid grey", margin: "5px" }}>
+          <center>
+            {studyList[activeCard].en}
+            <br />
+            <br />
+            {reveal && studyList[activeCard].gd}
+            <br />
+            <br />
+            {!reveal ? (
+              <button onClick={() => setReveal(!reveal)}>Show Answer</button>
+            ) : (
+              <>
+                <button onClick={handleRight}>Right</button>
+                <button onClick={handleWrong}>Wrong</button>
+              </>
+            )}
+          </center>
+        </div>
+      ) : (
+        <div style={{ border: "1px solid grey", margin: "5px" }}>
+          <center>You have finished this deck!</center>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
