6cf385d590fc38f739ccb65dc675f195d5338c11
Gordon Maloney <gordonmaloney@gmail.com>
2022-03-22 08:49:02 +0000

    select worder, scroller

--- src/components/PronunciationCenter/Player.js
+++ src/components/PronunciationCenter/Player.js
@@ -5,7 +5,6 @@ export const Player = ({ word }) => {
   return (
     <>
       <h2>{word}</h2>
-      <br />
 
       <audio src={`/Audio/${word}.mp3`} controls="controls" />
     </>
--- src/components/PronunciationCenter/PronunciationCentre.js
+++ src/components/PronunciationCenter/PronunciationCentre.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { RecordPlay } from "./RecordPlay";
 import { Player } from "./Player";
+import { WORDS } from "../Lessons/WORDS";
 
 export const PronunciationCentre = ({ lesson }) => {
-  const word = "chan eil";
+  const [word, setWord] = useState("toilichte");
 
   return (
     <div className="pronunciationCentre">
       <center>
         <h1>Pronunciation Centre</h1>
 
-        <i>Work in progress - to be continued...</i>
-
         <Player word={word} />
         <RecordPlay />
+
+        <hr />
+        <h4>Other words in lesson {lesson}</h4>
+        <div className="pronunciationCenterSelecter">
+        {WORDS.filter((level) => level.lesson == lesson)[0].words.map(
+          (word) => (
+            <>
+              <span onClick={() => setWord(word.gd)}>{word.gd}</span>
+              <br />
+            </>
+          )
+        )}
+        </div>
       </center>
     </div>
   );
